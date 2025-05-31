8628079dade4a301246ba6b556fe7fc5ea4a21ef
Omotola Oyeniyi <kingsx@localhost.localdomain>
2025-05-31 12:38:14 +0100

    updated images appropriately3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";  // <-- Changed here
 import { CartProvider } from "./hooks/useCart";
 import { AuthProvider } from "./hooks/useAuth";
 import Header from "./components/Header";
@@ -25,7 +24,7 @@ const App = () => (
         <CartProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>  {/* <-- Changed here */}
             <div className="min-h-screen bg-dark-gradient">
               <Header />
               <main>
@@ -41,7 +40,7 @@ const App = () => (
               </main>
               <Footer />
             </div>
-          </BrowserRouter>
+          </HashRouter>
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
